e4a9525b22b3336e3e8235736250fd4892104410
adrian-vatui <adrianvatui9989@yahoo.com>
2024-01-04 19:04:03 +0200

    updated SampleToken as per requirements

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -7,7 +7,7 @@ contract SampleToken {
     string public name = "Sample Token";
     string public symbol = "TOK";
 
-    uint256 public totalSupply;
+    uint256 private _totalSupply;
     
     event Transfer(address indexed _from,
                    address indexed _to,
@@ -17,40 +17,57 @@ contract SampleToken {
                    address indexed _spender,
                    uint256 _value);
 
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping(address => uint256)) public allowance;
+    mapping (address => uint256) private _balanceOf;
+    mapping (address => mapping(address => uint256)) private _allowance;
 
     constructor (uint256 _initialSupply) {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
-        emit Transfer(address(0), msg.sender, totalSupply);
+        _balanceOf[msg.sender] = _initialSupply;
+        _totalSupply = _initialSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
+        require(_balanceOf[msg.sender] >= _value);
+
+        _balanceOf[msg.sender] -= _value;
+        _balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
 
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
+        _allowance[msg.sender][_spender] = _value;
+
         emit Approval(msg.sender, _spender, _value);
-        allowance[msg.sender][_spender] = _value;
+
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        require(_value <= _balanceOf[_from]);
+        require(_value <= _allowance[_from][msg.sender]);
+
+        _balanceOf[_from] -= _value;
+        _balanceOf[_to] += _value;
+        _allowance[_from][msg.sender] -= _value;
 
         emit Transfer(_from, _to, _value);
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
+
         return true;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return _balanceOf[_owner];
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowance[_owner][_spender];
     }
 }
 
@@ -71,16 +88,27 @@ contract SampleTokenSale {
     }
 
     function buyTokens(uint256 _numberOfTokens) public payable {
-        require(msg.value == _numberOfTokens * tokenPrice);
-        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
-        require(tokenContract.transfer(msg.sender, _numberOfTokens));
-        emit Sell(msg.sender, _numberOfTokens);
+        uint256 amountToPay = _numberOfTokens * tokenPrice;
+        require(msg.value >= amountToPay);
+        require(tokenContract.allowance(owner, address(this)) >= _numberOfTokens);
+        require(tokenContract.transferFrom(owner, msg.sender, _numberOfTokens));
+
         tokensSold += _numberOfTokens;
+
+        payable(msg.sender).transfer(msg.value - amountToPay);
+
+        emit Sell(msg.sender, _numberOfTokens);
     }
 
     function endSale() public {
-        require(tokenContract.transfer(owner, tokenContract.balanceOf(address(this))));
         require(msg.sender == owner);
+        require(tokenContract.transfer(owner, tokenContract.balanceOf(address(this))));
         payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function setTokenPrice(uint256 _price) public {
+        require(msg.sender == owner);
+
+        tokenPrice = _price;
     }
 }
